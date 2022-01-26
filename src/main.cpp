e313e1c772dcc37f590441e31e8bd79cb7248498
beanpapac <rcsordas@unomaha.edu>
2022-01-26 12:30:50 -0600

    Cleaned up code and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,73 +8,50 @@
 
 
 Adafruit_7segment matrix = Adafruit_7segment();
+int tracker = 0;
+int cutsPerSec = 5;
+int pollHz = 100;
+
 
 void setup() {
 Serial.begin(921600);
 Serial.println("7 Segment Backpack Test");
 matrix.begin(0x70);
 pinMode(A7, INPUT);
-
 }
 
-int tracker = 0;
-void loop() {
-  int sensorValue = analogRead(A9);
-  //int pinval = analogRead(A7);
+int codeDelay = int(1000/pollHz);
+int readDelay = int(1000/cutsPerSec);
 
+void loop() {
+  //int sensorValue = analogRead(A9);
+  //int pinval = analogRead(A7);
   //Serial.println(pinval);
 
+  // if the circuit is completed and A7 is pulled high record a cut and sleep until the next cut can be recorded
   if(digitalRead(A7))
   {
     tracker++;
-    delay(200);
+    delay(readDelay);
+    matrix.print(tracker, DEC);
+    matrix.writeDisplay();
   }
-  if(tracker > 9000)
+  else
+  {
+    delay(codeDelay);
+    matrix.print(tracker, DEC);
+    matrix.writeDisplay();
+  }
+
+  // reset tracker value before it gets too large for display
+  if(tracker > 9997)
   {
     tracker = 0;
   }
 
-  // if(digitalRead(A7))
-  // {
-  //   tracker = 0;
-  //   Serial.println("true");
-  // }
-  // else
-  // {
-  //   //Serial.println("false");
-  // }
-
-  matrix.print(tracker, DEC);
-  matrix.writeDisplay();
-  delay(10);
-
   // Puts the Esp32 into deep sleep mode after 20 min, increases battery life by 40x
-  if(millis() > 1200000)
+   if(millis() > 1200000)
   {
     esp_deep_sleep_start();
   }
-  // boolean drawDots = true;
-  // // try to print a number thats too long
-  // matrix.print(10000, DEC);
-  // matrix.writeDisplay();
-  // delay(500);
-
-  // // print a hex number
-  // matrix.print(0xBEEF, HEX);
-  // matrix.writeDisplay();
-  // delay(500);
-
-  // // print a floating point 
-  // matrix.print(12.34);
-  // matrix.writeDisplay();
-  // delay(500);
-
-  // // print a string message
-  // matrix.println("BEef");
-  // matrix.drawColon(drawDots);
-  // matrix.writeDisplay();
-  // delay(100000);
- 
-  
-
 }
